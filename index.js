e66058a87938edec7da9d218af80d0483b4b2fc5
Raul Castillo <sx353553@yahoo.com>
2026-05-28 15:14:48 -0500

    update code

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 // Service ID: service_5ct4j2k
 // QuMhvyuhj3nsaupoV
 
-function contact() {
-    console.log ("It worked");
+function contact(event) {
+    event.preventDefault();
+    console.log("It worked");
 }
