05729cb1a9216c5466de8e02dbb018640d8a99ab
Raul Castillo <sx353553@yahoo.com>
2026-06-02 15:55:20 -0500

    update code

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 /// EmailJS SDK
+let isModalOpen = false;
+let contrastToggle = false;
+
+
+function toggleContrast() {
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList. += "dark-theme";
+  }
+  else {
+    document.body.classList.remove("dark-theme");
+  }
+}
 
 function contact(event) {
   event.preventDefault();
