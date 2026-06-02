4ba2bf9efa636e2b35a97cc4eabf4509cc2ed4df
Raul Castillo <sx353553@yahoo.com>
2026-06-02 09:44:56 -0500

    update code

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ let isModalOpen = false;
 function toggleModal() { 
     if (isModalOpen) {
         isModalOpen = false;
-        return document.body.classList.remove(" modal--open");
+        return document.body.classList.remove("modal--open");
     }
       isModalOpen =true; 
    document.body.classList += " modal--open";
