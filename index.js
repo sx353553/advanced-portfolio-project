0a2a273e18675c1e4f1307ced470a2fe7a326541
Raul Castillo <sx353553@yahoo.com>
2026-06-02 15:35:55 -0500

    update code

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
 /// EmailJS SDK
 
 function contact(event) {
-    event.preventDefault();
-    const loading = document.querySelector(".modal__overlay--loading");
-    const success = document.querySelector(".modal__overlay--success");
-    loading.classList += " modal__overlay--visible";
-    emailjs.sendForm("service_5ct4j2k", "template_xrrxtto", event.target, "QuMhvyuhj3nsaupoV")
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+  emailjs
+    .sendForm(
+      "service_5ct4j2k",
+      "template_xrrxtto",
+      event.target,
+      "QuMhvyuhj3nsaupoV",
+    )
     .then(() => {
-        loading.classList.remove("modal__overlay--visible");
-        success.classList += " modal__overlay--visible";
-    }).catch(() => {
-        loading.classList.remove("modal__overlay--visible");
-        alert(
-            "The email service is temporarily unavailable. Please contact me directly on my     email.rchunt60@gmail.com"
-        );
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact me directly on my email: rchunt60@gmail.com"
+      );
     });
-} 
+}
 let isModalOpen = false;
-function toggleModal() { 
-    if (isModalOpen) {
-        isModalOpen = false;
-        return document.body.classList.remove("modal--open");
-    }
-      isModalOpen =true; 
-   document.body.classList += " modal--open";
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
 }
